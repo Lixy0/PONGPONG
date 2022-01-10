a785278fb4e04580cc27b4293ced46ea9b66ca22
Lixy0 <lixygame@gmail.com>
2022-01-10 15:57:24 +0100

    Pong 1er Push

--- src/Tableau.js
+++ src/Tableau.js
@@ -1,12 +1,107 @@
 class Tableau extends Phaser.Scene {
+
+    /**
+     * Précharge les assets
+     */
     preload() {
-        this.load.image('square','asset/carre.png');
-        this.load.image('circle','asset/cercle.png');
+        this.load.image('carre','asset/carre.png');
+        this.load.image('balle','asset/cercle.png');
     }
+
+    /**
+     * Affiche les assets/placement/size
+     */
+
     create() {
-        this.square = this.add.image(0,0,'square').setOrigin(0,0);
-        this.circle = this.add.image(0,0,'circle').setOrigin(0,50);
+        this.hauteur=500
+        this.largeur=1000
+
+        //haut (physique, taille)
+        this.haut = this.physics.add.sprite(0,0,'carre').setOrigin(0,0);
+        this.haut.setDisplaySize(this.largeur,20);
+        this.haut.body.setAllowGravity(false);
+        this.haut.setImmovable(true);
+        //bas(physique, taille)
+        this.bas = this.physics.add.sprite(0,this.hauteur-20,'carre').setOrigin(0,0);
+        this.bas.setDisplaySize(this.largeur,20);
+        this.bas.body.setAllowGravity(false);
+        this.bas.setImmovable(true);
+
+        //affichage ball
+        this.balle = this.physics.add.sprite(this.largeur/2,this.hauteur/2,'balle').setOrigin(0,0);
+        this.balle.setDisplaySize(20,20);
+        //actions de rebondisement/speed
+        this.balle.body.setBounce(1.1,1.1);
+        this.balle.setVelocityX(Phaser.Math.Between(200,-200));
+        this.balle.body.setMaxVelocity(500,500)
+
+
+        //gauche(physique, taille)
+        this.gauche = this.physics.add.sprite(40,210,'carre').setOrigin(0,0);
+        this.gauche.setDisplaySize(15,75);
+        this.gauche.body.setAllowGravity(false);
+        this.gauche.setImmovable(true);
+
+        //droite(physique, taille)
+        this.droite = this.physics.add.sprite(950,210,'carre').setOrigin(0,0);
+        this.droite.setDisplaySize(15,75);
+        this.droite.body.setAllowGravity(false);
+        this.droite.setImmovable(true);
+
+        //colliders
+        this.physics.add.collider(this.balle,this.haut);
+        this.physics.add.collider(this.balle,this.bas);
+        this.physics.add.collider(this.balle,this.gauche);
+        this.physics.add.collider(this.balle,this.droite);
+        this.physics.add.collider(this.droite,this.haut);
+        this.physics.add.collider(this.droite,this.bas);
+        this.physics.add.collider(this.gauche,this.haut);
+        this.physics.add.collider(this.gauche,this.bas);
+
+
+
+
+        //création de la fonction clavier
+        this.initKeyboard();
+        this.speed=0;
     }
+
     update() {
+        if (this.balle.x > this.largeur) {
+            this.balle.x = 0
+        }
+        if (this.balle.y < 0) {
+            this.balle.y = 0
+        }
+        if (this.balle.y > this.hauteur) {
+            this.balle.y = 0
+        }
+    }
+
+    initKeyboard(){
+        let me=this
+        this.input.keyboard.on('keydown', function (kevent) {
+            switch (kevent.keyCode) {
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.UP:
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.DOWN:
+                    break;
+            }
+        });
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                case Phaser.Input.Keyboard.KeyCodes.UP:
+                case Phaser.Input.Keyboard.KeyCodes.DOWN:
+                    break;
+            }
+        });
     }
     }
--- src/main1.js
+++ src/main1.js
@@ -1,15 +1,15 @@
 let gameConfig = {
     type: Phaser.AUTO,
-    width: 960,
-    height: 540,
-    backgroundColor: 'rgba(76,138,97,0.87)',
+    width: 1000,
+    height: 500,
+    backgroundColor: 'rgba(0,0,0,0.87)',
     parent: 'game',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y:  200}
         }
     },
-    scene: new Tableau1()
+    scene: new Tableau()
 };
 let game = new Phaser.Game(gameConfig);
